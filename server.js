8f4a82c482ba1c3f2158916e324f2075a8b5c0e7
drmeloy <drmeloy@gmail.com>
2019-12-07 17:55:31 -0700

    server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
-http.createServer( (req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write('Hello world');
-  res.end();
-}).listen(8888);
+const start = () => {
+  const onRequest = (req, res) => {
+    console.log('Request received');
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write('Hello world');
+    res.end();
+  };
+  
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started');
+};
+
+module.exports = start;
