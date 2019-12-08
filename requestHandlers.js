0572b58d321bac1f0b31a269147d4f508646d70c
drmeloy <drmeloy@gmail.com>
2019-12-08 12:01:37 -0700

    html

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,20 +3,20 @@ const exec = require('child_process').exec;
 const start = (response) => {
   console.log('Request handler "start" was called.');
 
-  const body = /*html*/ `
-    <html>
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    </head>
-    <body>
-    <form action='/upload' method='post'>
-    <input type='submit' value='Submit text' />
-    </form>
-    </body>
-    </html>
-  `;
-  
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  const body = '<html>'+
+  '<head>'+
+  '<meta http-equiv="Content-Type" content="text/html; '+
+  'charset=UTF-8" />'+
+  '</head>'+
+  '<body>'+
+  '<form action="/upload" method="post">'+
+  '<textarea name="text" rows="20" cols="60"></textarea>'+
+  '<input type="submit" value="Submit text" />'+
+  '</form>'+
+  '</body>'+
+  '</html>';
+
+  response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
   response.end();
 }
