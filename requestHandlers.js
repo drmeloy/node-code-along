28a6b4d25b6c95a65397d3058a9af63b7b275e2b
drmeloy <drmeloy@gmail.com>
2019-12-08 10:51:23 -0700

    requesthandlers

---
+++ requestHandlers.js
@@ -0,0 +1,9 @@
+const start = () => {
+  console.log('Request handler "start" was called.');
+}
+
+const upload = () => {
+  console.log('Request handler "upload" was called.');
+}
+
+module.exports = { start, upload };
