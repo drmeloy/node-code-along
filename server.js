a8d59ab224ce26666db19f41a34ac7a9fcd7ef74
drmeloy <drmeloy@gmail.com>
2019-12-08 10:24:10 -0700

    index

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const http = require('http');
+const url = require('url');
 
 const start = () => {
   const onRequest = (req, res) => {
-    console.log('Request received');
+    const pathname = url.parse(request.url).pathname;
+    console.log('Request for' + pathname + 'received.');
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.write('Hello world');
     res.end();
-  };
+  }
   
   http.createServer(onRequest).listen(8888);
   console.log('Server has started');
-};
-
-module.exports = start;
+}
+  
+module.exports = { start };
