1961284e774bff4107ed726ca8a5f6475708ca7c
drmeloy <drmeloy@gmail.com>
2019-12-08 11:54:05 -0700

    html rendering

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,11 +3,22 @@ const exec = require('child_process').exec;
 const start = (response) => {
   console.log('Request handler "start" was called.');
 
-  exec('ls -lah', (error, stdout, stderr) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout);
-    response.end();
-  });
+  const body = /*html*/ `
+    <html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+    <form action='/upload' method='post'>
+    <input type='submit' value='Submit text' />
+    </form>
+    </body>
+    </html>
+  `;
+  
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write(body);
+  response.end();
 }
 
 const upload = (response) => {
