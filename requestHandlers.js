51343282b92db98302fc34af0dc510435f20eaba
drmeloy <drmeloy@gmail.com>
2019-12-08 19:33:54 -0700

    I did it!

--- requestHandlers.js
+++ requestHandlers.js
@@ -17,7 +17,7 @@ const start = (response) => {
   </form>
   </body>
   </html>
-  `
+  `;
 
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
@@ -26,29 +26,31 @@ const start = (response) => {
 
 const upload = (response, request) => {
   console.log('Request handler "upload" was called.');
+
   const form = new formidable.IncomingForm();
   console.log('About to parse');
   form.parse(request, (error, fields, files) => {
     console.log('parsing done');
-    fs.unlink('./assets/test.JPG', error => {
-      console.log(error);
-    });
-    console.log('Unlink done');
+
     fs.rename(files.upload.path, 'C:\\Users\\Dan\\Documents\\alchemy\\career-track\\labs\\node-code-along\\assets\\test.JPG', error => {
-      console.log(error);
-    });
-    console.log('Rename done');
+      if(error){
+        fs.unlink('./assets/test.JPG', error => {
+          console.log(error);
+        });
+        fs.rename(files.upload.path, 'C:\\Users\\Dan\\Documents\\alchemy\\career-track\\labs\\node-code-along\\assets\\test.JPG');
+      }
     });
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Received image:<br/>")
+  response.write("Received image:<br/>");
   response.write("<img src='/show' />");
   response.end();
+});
 }
 
 const show = response => {
   console.log('Requeset handler "show" was called.');
   response.writeHead(200, {"Content-Type": "image/JPG"});
   fs.createReadStream("./assets/test.JPG").pipe(response);
-}
+};
 
 module.exports = { start, upload, show };
